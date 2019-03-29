ef4ba62983c1f8be1dbd3c2ff8c0a683a8ddd5e2
Sidney Vidal <sidneyferreiravidal@hotmail.com>
2019-03-29 12:36:07 -0300

    Initial commit

---
+++ PA2-edu/telaInicial.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Eduardo Lima
+ * Date: 22/03/2019
+ * Time: 19:06
+ */ ?>
+</body>
+</html>
